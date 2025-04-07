b54186933e1a15c59b01a69bed8e619ed7eb642f
Sorokin Konstantin <konstantin.69.sorokin@gmail.com>
2025-04-07 17:01:48 +0300

    feat: create config file and env.template

---
+++ src/utils/config.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,
+)
+
+
+class Bot(BaseModel):
+    token: str = ""
+    admins: int = int()
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(
+            ".env.template",
+            ".env",
+        ),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
+    bot: Bot = Bot()
+
+
+settings = Settings()
